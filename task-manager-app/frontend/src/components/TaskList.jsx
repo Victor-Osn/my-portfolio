a5ac97d9fc3564f1dde981c6b9e362a217473911
Victor Osaikhuwuomwan <osasv35@gmail.com>
2026-03-18 20:08:36 -0400

    Update TaskList.jsx

--- task-manager-app/frontend/src/components/TaskList.jsx
+++ task-manager-app/frontend/src/components/TaskList.jsx
@@ -1,8 +1,13 @@
+import TaskItem from "./TaskItem";
+
 function TaskList() {
   return (
     <div>
       <h2>Task List</h2>
       <p>This component will display all tasks.</p>
+
+      {/* Temporary sample task item */}
+      <TaskItem />
     </div>
   );
 }
