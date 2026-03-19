757d9b7f851460537e7b8cf6168b7b96ca0dd71d
Victor Osaikhuwuomwan <osasv35@gmail.com>
2026-03-18 20:04:47 -0400

    Update App.jsx

--- task-manager-app/frontend/src/App.jsx
+++ task-manager-app/frontend/src/App.jsx
@@ -1,8 +1,15 @@
+import "./Styles/app.css";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+
 function App() {
   return (
     <div>
       <h1>Task Manager App</h1>
       <p>Version 1 frontend setup in progress.</p>
+
+      <TaskForm />
+      <TaskList />
     </div>
   );
 }
